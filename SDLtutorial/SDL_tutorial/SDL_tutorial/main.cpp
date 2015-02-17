ecb37af50929c0e035699d3c72dc9b7ef41f7ac2
Bram Joris Grooten <grootenb@wit.edu>
2015-02-17 11:00:33 -0500

    I finished lab 3 about a week ago. Now finally committing it to GitHub.

---
+++ SDLtutorial/SDL_tutorial/SDL_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_tutorial
+//
+//  Created by Bram Joris Grooten on 2/17/15.
+//  Copyright (c) 2015 Bram Joris Grooten. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
